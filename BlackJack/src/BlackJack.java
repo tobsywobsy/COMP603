6d2ca3afbadc8ff792cd42cca89c1211e9d80a92
tobsywobsy <trevretubalado@gmail.com>
2024-04-04 14:52:40 +1300

    Drawing cards

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -17,25 +17,129 @@ public class BlackJack {
         public String toString() {
             return value+ "-" + type;
         }
+
+        public int getValue() {
+            if ("AJQK".contains(value)) { //Values A J Q K
+                if (value == "A") {
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value); //Values 2 - 10
+        }
+
+        public boolean isAce() {
+            return value == "A";
+        }
     }
 
     ArrayList<Card> deck;
     Random random = new Random(); //Shuffles the deck randomly
 
+    //Dealer
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;
+
+    //Player
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
+
+    //Window
+    int boardWidth = 600;
+    int boardHeight = boardWidth;
+
+    int cardWidth = 110; //1:1.4 ratio for best img resolution
+    int cardHeight = 154;
+
+    JFrame frame = new JFrame("Black Jack");
+    JPanel gamePanel = new JPanel() {
+        @Override
+        public void paintComponent(Graphics g) { //Drawing
+            super.paintComponent(g);
+
+            //Draw hidden card
+            Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
+            g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
+        }
+    };
+    JPanel buttonPanel = new JPanel();
+    JButton hitButton = new JButton("Hit");
+    JButton stayButton = new JButton("Stay");
+
+    //Constructor
     BlackJack() {
         startGame();
-    }
 
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gamePanel.setLayout(new BorderLayout());
+        gamePanel.setBackground(new Color(50, 120, 80));
+        frame.add(gamePanel);
+
+        buttonPanel.setBackground(new Color(50, 50, 50));
+        hitButton.setFocusable(false);
+        buttonPanel.add(hitButton);
+        stayButton.setFocusable(false);
+        buttonPanel.add(stayButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+    }
+    
+    //Function for starting the game
     public void startGame() {
         //Deck
         buildDeck();
         shuffleDeck();
+
+        //Dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.remove(deck.size()-1); //Removes card at last index
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.remove(deck.size()-1);
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+        
+        System.out.println("Dealer:");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+
+        //Player
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for (int i = 0; i <2; i++) {
+            card = deck.remove(deck.size()-1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
+
+        System.out.println("Player:");
+        System.out.println(playerHand);
+        System.out.println(playerSum);
+        System.out.println(playerAceCount);
+
     }
 
     public void buildDeck() {
         deck = new ArrayList<Card>();
         String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        String[] types = {"C", "S", "H", "D"};
+        String[] types = {"C", "D", "H", "S"};
 
         for (int i = 0; i < types.length; i++) {
             for (int j = 0; j < values.length; j++) {
@@ -44,7 +148,7 @@ public class BlackJack {
             }
         }
 
-        System.out.println("Building Deck:");
+        System.out.println("Building Deck...");
         System.out.println(deck);
     }
 
